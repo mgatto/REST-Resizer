f8b10c83dda85439e8c72b99ac0ba0756d236c3d
Michael Gatto <mgatto@lisantra.com>
2013-08-07 23:38:03 -0700

    * Validate parameters.

--- cpanfile
+++ cpanfile
@@ -1,3 +1,4 @@
-requires 'Modern::Perl', '>= 1.20121103';
-requires 'Mojolicious',  '>= 4.23';
-requires 'Imager',       '>= 0.97';
+requires 'Modern::Perl',        '>= 1.20121103';
+requires 'Mojolicious',         '>= 4.23';
+requires 'Imager',              '>= 0.97';
+requires 'use Regexp::Common;'  '>= 2013031301';
--- resizer.pl
+++ resizer.pl
@@ -3,10 +3,11 @@ use Modern::Perl '2012';
 use Mojolicious::Lite;
 use Mojo::URL;
 use Mojo::UserAgent;
-use Mojo::Util qw(url_escape url_unescape);
-use URI;
+use Mojo::Util qw/ url_escape url_unescape /;
 use File::Basename; # core; no install...
 use Imager;
+use Regexp::Common qw/ number /;
+
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
@@ -40,10 +41,18 @@ get '/api/v1/resize' => sub {
 
     my $image_url = Mojo::URL->new(url_unescape $self->param('url'));
     my $image_name = basename($image_url->path);
+
     my $new_width = $self->param('w');
     my $new_height = $self->param('h');
 
-    # validate the integers
+    ## Validate width & height
+    return $self->render(
+        json => {error => sprintf("Bad Parameter: Width was %s; must be whole number or decimal", $new_width)}, status => 500
+    ) unless $RE{num}{decimal}->matches($new_width);
+
+    return $self->render(
+        json => {error => sprintf("Bad Parameter: Height was %s; must be whole number or decimal", $new_width)}, status => 500
+    ) unless $RE{num}{decimal}->matches($new_height);
 
 
     # get the resource; better to use a real UA string so servers in the wild
@@ -52,60 +61,48 @@ get '/api/v1/resize' => sub {
         ->name('"Mozilla/5.0 (Windows NT 5.1; rv:22.0) Gecko/20100101 Firefox/22.0"')
         ->request_timeout(5);
 
-    # set the accept header for mime types which Imager can process
+    # Docs say: "When downloading large files with Mojo::UserAgent you
+    # don't have to worry about memory usage at all, because it will
+    # automatically stream everything above 250KB into a temporary file."
+    #
+    # Set the accept header for mime types which Imager can process
     my $tx = $grabber->get($image_url->to_string, {'Accept' => 'image/jpeg, image/gif, image/png'});
-        # res is an instance of Mojo::Message::Response
-        #
-        # Docs say: "When downloading large files with Mojo::UserAgent you
-        #don't have to worry about memory usage at all, because it will
-        #automatically stream everything above 250KB into a temporary file."
     return $self->render(
         json => {error => "Cannot get image: " .  $tx->error}, status => 500
     ) if $tx->error;
 
+    # Move it to somewhere from which we can serve it later.
+    #
+    # `res` is an instance of Mojo::Message::Response
     my $downloaded_img = $tx->res->content->asset->move_to('public/' . $image_name);   #->body();
     return $self->render(
         json => {error => "Cannot move image: " .  $tx->error}, status => 500
     ) if $tx->error;
 
-    # attempt to resize it
-    # read during init: Imager >= 0.68
+    ## Resize it!
     my $img = Imager->new();
-    #$img->read(data => $downloaded_img);
     $img->read(file => 'public/' . $image_name);
     return $self->render(json => {error => "Cannot read image: " . $img->errstr}, status => 500) if $img->errstr;
 
+    # non-proportional x or y pixels don't matter; Imager handles it according to the `type` arg
     my $resized_img = $img->scale(
         xpixels => $new_width,
         ypixels => $new_height,
-        type    => 'max',   # default
+        type    => 'max',   # default = use the max size if x,y are not proportional
         qtype   => 'normal' # default
     );
-    # my $type = optional
 
-## OPERATE ONLY VIA FILEHANDLES?
-
-    # return as a stream
-    #binmode STDOUT; #for CGI; mojo may not need
-    my $new_image_ref;
     $resized_img->write(
-        #type => $type, optional
-        #data => \$new_image_ref,
         file => 'public/' . $image_name,
-        #fd   => fileno(STDOUT)
     );
     return $self->render(json => {error => "Cannot write image: " . $img->errstr()}, status => 500) if $img->errstr();
 
-    #return $self->render(data => $new_image_ref);
-    # we need to manually set a static serving path
-    #push @{$self->app->static->paths}, 'public';
+    ## Return directly to client
+    $self->render_static($image_name); # default is to serve from ./public
 
-    #my $static = $self->app->static();
-    #push @{$static->paths}, 'public';
-    $self->render_static($image_name);
-
+    ## Don't fill up server with images, nor keep them around lest others get them.
     unlink 'public/' . $image_name
-        or warn "Could not delete resized image: $image_name";
+        or $self->log->warn("Could not delete resized image: $image_name");
 };
 
 
